934272d03d6b3552abbd70a3dea8f8110a4c323b
khanghoang2407vn-cmd <khanghoang2407.vn@gmail.com>
2026-09-17 12:40:16 +0700

    Update Person.java

--- src/model/Person.java
+++ src/model/Person.java
@@ -11,4 +11,64 @@ import java.io.Serializable;
  */
 public abstract class Person implements Serializable {
 
+    private int id;
+    private String code;
+    private String name;
+    private String phoneNumber;
+    private String role;
+
+    public Person() {
+    }
+
+    public Person(int id, String code, String name, String phoneNumber, String role) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public abstract void displayDetails();
+
+    public abstract String toCsvLine();
 }
